ee615a2a6df0d8dc8f41258af9708a4c51b955a9
Benjamin Tan <ben@witsvale.com>
2011-09-30 14:47:38 +0800

    completed first problem set

--- CS2104/PS4/ps0401.pl
+++ CS2104/PS4/ps0401.pl
@@ -1,35 +1,101 @@
+% Benjamin Tan Wei Hao
+% U077129N
 % Fractal Fun
 
-:-op(900,yf,;).
+% Behavior is similar to how the '+' operator works
+:- op(499,xfy,;).
 
 % Main Predicate to call
-fractal(X = (X), _Level, _Length) :-
-	writeln('Base Case'), !.
-
 fractal(X = (Expr), Level, Length) :-
+	writeln('from turtle import *'),
+	writeln('import time'),
+	% writeln('s = Screen()'),
+	% writeln('s.setworldcoordinates(0,-500, 1500, 1000)'),
+	writeln('delay(0)'),
 	fractal(X, Expr, Level, Length, Expr).
 
-fractal(_X, _, 0, Length, _StoredExpr) :-
-	write('forward('), write(Length), writeln(')').
- 
-% X      	 : Graph object (Used to check the equation for syntax errors)
-% Expr   	 : Remaining expression
-% StoredExpr : Stored Expression that we used.
-fractal(X, E;Ee, Level, Length, StoredExpr) :-
-	E =.. [right, Angle],
-	write('right('), write(Angle), writeln(')'),
+fractal(X, X;Ee, Level, Length, StoredExpr) :-
+	Level > 1,
+	Level1 is Level-1, 
+	% Recurse one level down
+	fractal(X, StoredExpr, Level1, Length, StoredExpr),
 	fractal(X, Ee, Level, Length, StoredExpr).
 
+
 fractal(X, E;Ee, Level, Length, StoredExpr) :-
+	Level > 1, 
 	E =.. [left, Angle],
 	write('left('), write(Angle), writeln(')'),
 	fractal(X, Ee, Level, Length, StoredExpr).
 
-fractal(X, E;_, Level, Length, StoredExpr) :-
-	writeln(E),
-	E = X, 
+
+fractal(X, E;Ee, Level, Length, StoredExpr) :-
+	Level > 1, 
+	E =.. [right, Angle], 
+	write('right('), write(Angle), writeln(')'),
+	fractal(X, Ee, Level, Length, StoredExpr).
+
+
+fractal(_X, E, Level, _Length, _StoredExpr) :-
+	Level > 1, 
+	E =.. [left, Angle],
+	write('left('), write(Angle), writeln(')').
+
+
+fractal(_X, E, Level, _Length, _StoredExpr) :-
+	Level > 1, 
+	E =.. [right, Angle], 
+	write('right('), write(Angle), writeln(')').
+
+
+fractal(X, X, Level, Length, StoredExpr) :-
+	Level > 1, 
 	Level1 is Level-1,
 	fractal(X, StoredExpr, Level1, Length, StoredExpr).
 
-:- fractal(g = (g;left(45);g),3,30).
-% :- fractal(g = (g;left(45);g;right(90);g;left(45);g),3,30).
+
+fractal(X, _Expr, 1, Length, StoredExpr) :-
+	fractal_helper(X, StoredExpr, Length).
+
+fractal(_X, _Expr, _, _Length, _StoredExpr).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% helper predicates to build one "unit"
+fractal_helper(X, X;Ss, Length) :-
+	write('forward('), write(Length), writeln(')'),
+	fractal_helper(X, Ss, Length).
+
+fractal_helper(X, S;Ss, Length) :-
+	S =.. [left, Angle], 
+	write('left('), write(Angle), writeln(')'),
+	fractal_helper(X, Ss, Length).
+
+fractal_helper(X, S;Ss, Length) :-
+	S =.. [right, Angle], 
+	write('right('), write(Angle), writeln(')'),
+	fractal_helper(X, Ss, Length).
+
+fractal_helper(_X, S, _Length) :-
+	S =.. [left, Angle], 
+	write('left('), write(Angle), writeln(')').
+
+fractal_helper(_X, S, _Length) :-
+	S =.. [right, Angle], 
+	write('right('), write(Angle), writeln(')').
+
+fractal_helper(X, X, Length) :-
+	write('forward('), write(Length), writeln(')').
+
+
+% :- tell('run.py'),
+%    fractal(g=(left(30);g;right(120);g;left(120);g;right(30)),5,20), 
+%    writeln('time.sleep(1000)'), told.
+
+
+:- tell('run.py'),
+   fractal(g = (left(120);g;right(60);g;right(60);g;right(60);g;g;left(60)),2,10), 
+   writeln('time.sleep(1000)'), told.
+
+% :- tell('run.py'),
+%    fractal(g = (g; left(45);g;right(90);g;left(45);g),4,5), 
+%    writeln('time.sleep(1000)'), told.
