e62c8076dfd26258a899a699cf60a9fa802f6c00
Benjamin Tan <ben@witsvale.com>
2011-10-20 22:59:39 +0800

    changed implementation to return a list instead of using foldl

--- CS2104/Tut8/ex1.hs
+++ CS2104/Tut8/ex1.hs
@@ -1,3 +1,11 @@
+--Power Series
 module Main where
-    expo num = foldl (\acc depth -> acc + ((num ** depth) / (product [1..depth]))) 1
+    --expo num = foldl (\acc depth -> acc + ((num ** depth) / (product [1..depth]))) 1
+   --This defines one term of an exponential power series function 
+   expo num = map (\depth -> ((num ** depth) / (product [1..depth]))) 
    
+
+--Write a Haskell function that takes in an infinite stream that represents a power series, 
+--and a value for variable x, and computes an infinite stream of approximations to the value 
+--of the series for the given argument. Test your function on the stream defined in your 
+--solution to Exercise 1.
