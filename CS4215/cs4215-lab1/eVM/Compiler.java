30a910d086d46bd1d6d1a1a3ad30e26e7c50e1b5
Benjamin Tan <ben@witsvale.com>
2012-01-24 02:59:13 +0800

    Done 4 operators

--- CS4215/cs4215-lab1/eVM/Compiler.java
+++ CS4215/cs4215-lab1/eVM/Compiler.java
@@ -1,4 +1,4 @@
-package eVM; 
+package eVM;
 
 import ePL.*;
 import eVML.*;
@@ -7,74 +7,75 @@ import java.util.*;
 
 public class Compiler {
 
-    private Vector <INSTRUCTION> instructions;
+	private Vector<INSTRUCTION> instructions;
 
-    private Expression expression;
+	private Expression expression;
 
-    public Compiler(Expression exp) {
-	expression = exp;
-    }
+	public Compiler(Expression exp) {
+		expression = exp;
+	}
 
-   // compile
+	// compile
 
-   public INSTRUCTION[] compile() {
-      instructions = new Vector <INSTRUCTION> ();
-      comp(expression);
-      instructions.add(new DONE());
-      return instructions.toArray(new INSTRUCTION[1]);
-   }
+	public INSTRUCTION[] compile() {
+		instructions = new Vector<INSTRUCTION>();
+		comp(expression);
+		instructions.add(new DONE());
+		return instructions.toArray(new INSTRUCTION[1]);
+	}
 
-   // comp         
+	// comp
 
-   private void comp(Expression exp) {
+	private void comp(Expression exp) {
 
-      if (exp instanceof UnaryPrimitiveApplication) 
-      {
-	comp(((UnaryPrimitiveApplication)exp).argument);
-        instructions.add(instructionFromOp(((UnaryPrimitiveApplication)exp)
-                                           .operator));
-      }
-      if (exp instanceof BinaryPrimitiveApplication) 
-      {
-	comp(((BinaryPrimitiveApplication)exp).argument1);
-	comp(((BinaryPrimitiveApplication)exp).argument2);
-        instructions.add(instructionFromOp(((BinaryPrimitiveApplication)exp)
-                                           .operator));
-      }
-      else if (exp instanceof BoolConstant)
-      {
-        instructions.add(new 
-            LDCB(((BoolConstant)exp).value));
-      }
-      else if (exp instanceof IntConstant)
-      {
-        instructions.add(new 
-            LDCI(((IntConstant)exp).value));
-      }
-      else // (exp instanceof NotUsed)
-      { 
-        // skip
-      }
-   }
+		if (exp instanceof UnaryPrimitiveApplication) {
+			comp(((UnaryPrimitiveApplication) exp).argument);
+			instructions
+					.add(instructionFromOp(((UnaryPrimitiveApplication) exp).operator));
+		}
+		if (exp instanceof BinaryPrimitiveApplication) {
+			comp(((BinaryPrimitiveApplication) exp).argument1);
+			comp(((BinaryPrimitiveApplication) exp).argument2);
+			instructions
+					.add(instructionFromOp(((BinaryPrimitiveApplication) exp).operator));
+		} else if (exp instanceof BoolConstant) {
+			instructions.add(new LDCB(((BoolConstant) exp).value));
+		} else if (exp instanceof IntConstant) {
+			instructions.add(new LDCI(((IntConstant) exp).value));
+		} else // (exp instanceof NotUsed)
+		{
+			// skip
+		}
+	}
 
-    public static void displayInstructionArray(INSTRUCTION[] is) {
-      int i = 0;
-      while (! (is[i] == null)) 
-         System.out.println(i + " " + is[i++]);
-   }
+	public static void displayInstructionArray(INSTRUCTION[] is) {
+		int i = 0;
+		while (!(is[i] == null))
+			System.out.println(i + " " + is[i++]);
+	}
 
-   private static INSTRUCTION instructionFromOp(String op) {
-      if      (op.equals("+"))        { return new PLUS(); }
-      else if (op.equals("-"))        { return new MINUS(); }
-      else if (op.equals("*"))        { return new TIMES(); }
-      else if (op.equals("/"))        { return new DIV(); }
-      else if (op.equals("\\"))       { return new NOT(); }
-      else if (op.equals("&"))        { return new AND(); }
-      else if (op.equals("|"))        { return new OR(); }
-      else if (op.equals("<"))        { return new LT(); }
-      else if (op.equals(">"))        { return new GT(); }
-      else /* (op.equals("="))    */  { return new EQ(); }
-   }
-
+	private static INSTRUCTION instructionFromOp(String op) {
+		if (op.equals("+")) {
+			return new PLUS();
+		} else if (op.equals("-")) {
+			return new MINUS();
+		} else if (op.equals("*")) {
+			return new TIMES();
+		} else if (op.equals("/")) {
+			return new DIV();
+		} else if (op.equals("\\")) {
+			return new NOT();
+		} else if (op.equals("&")) {
+			return new AND();
+		} else if (op.equals("|")) {
+			return new OR();
+		} else if (op.equals("<")) {
+			return new LT();
+		} else if (op.equals(">")) {
+			return new GT();
+		} else /* (op.equals("=")) */{
+			return new EQ();
+		}
+	}
 
 }
