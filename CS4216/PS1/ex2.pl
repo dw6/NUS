e9539c02b77bbad588db9a62ff2dafd8313b3eb3
Benjamin Tan <ben@witsvale.com>
2012-02-10 01:20:34 +0800

    able to produce augmented matrix

--- CS4216/PS1/ex2.pl
+++ CS4216/PS1/ex2.pl
@@ -10,6 +10,22 @@ n_term(InX-InY, OutX-OutY) :- !,
 eqn(InX+InY=Z, OutX+OutY=Z) :- !, n_term(InX,OutX), n_term(InY,OutY).
 eqn(InX-InY=Z, OutX-OutY=Z) :- !, n_term(InX,OutX), n_term(InY,OutY).
 
+
+
+
+
+%%%%
+% Creates an augmented matrix based on the equations.
+%
+% ?- aug_rows([-1*X-2*B+3*C-4*D-5*R, -4*A-6*R-6*T+5*B], Out).
+%    Out = [[-1, -2, 3, -4, -5, 0, 0], [0, 5, 0, 0, -6, -4, -6]].
+aug_rows(Eqn,AugMatrix) :- !, get_vars(Eqn, V), aug_rows(Eqn,V,AugMatrix).
+aug_rows([E|ET],V,[C|CT]) :- !, aug_row(E,V,C), aug_rows(ET,V,CT). 
+aug_rows([],_,[]) :- !.
+
+aug_row(Eqn,[V|VT],[C|CT]) :- !, get_coeff(V,Eqn,C), aug_row(Eqn,VT,CT).
+aug_row(_,[],[]) :- !.
+
 % Collect unique variables from each of the equations.
 get_vars(InL,OutL) :- !, get_vars(InL, [], AuxL), sort(AuxL,OutL). 
 get_vars(N*C,InL,OutL) :- number(N), var(C), append([C],InL, OutL).
@@ -19,32 +35,20 @@ get_vars(Terms=_, InL, OutL) :- !, get_vars(Terms,InL,OutL).
 get_vars([H|T],InL,OutL) :- !, get_vars(H,InL,AuxL), get_vars(T,AuxL, OutL).
 get_vars([],L,L) :- !.
 
-
-
-
-% sort_eqns(InL,[HOut|TOut]) :-
-% 	get_vars(InL,V), InL =..[H|T], sort_eqns(H,V,HOut), sort_eqns(T,V,TOut). 
-
-% sort_eqns(Eqn, Out) :- !, get_vars(Eqn,V), sort_eqns(Eqn,V, Out).
-
-% get_terms(Eqn, [VH|VT], Out) :-
-% 	!, get_term(Eqn,VH, A), writeln(A), get_terms(Eqn, VT, OutB), 
-% 	writeln(OutB),
-% 	Out =..[+, A, OutB].
-% get_terms(_, [], _) :- !.
-
-get_coeff(Var1,N*Var2,N) :- (Var1 == Var2),!.
-get_coeff(Var1,_ + N*Var2,N) :- (Var1 == Var2),!.
-get_coeff(Var,Rest + _,N) :- get_coeff(Var,Rest,N),!.
+%%%%
+% Retrieve coefficients of the equation given the (sorted) variables list.
+% 
+% ?- get_coeff(B, -1*X-2*B+3*C, Out).
+% Out = -2.
+get_coeff(V1,N*V2,N) :- V1 == V2, !.
+get_coeff(V1,_A+N*V2,N) :- V1 == V2, !.
+get_coeff(V1,_A-N*V2,N1) :- V1 == V2, N1 is N * -1, !.
+get_coeff(Var,A+_B,N) :- get_coeff(Var,A,N),!.
+get_coeff(Var,A-_B,N) :- get_coeff(Var,A,N),!.
 get_coeff(_,_,0).
 
 
-
-augrows([E|ET],V,[C|CT]) :- !, augrow(E,V,C), augrows(ET,V,CT). 
-augrows([],_,[]) :- !.
-
-augrow(Eqn, [V|VT], [C|CT]) :- !, get_coeff(V,Eqn,C), augrow(Eqn,VT,CT).
-augrow(_Eqn, [], []) :- !.	
+	
 
 % solve_lin( [ X*2+Y=3, 3*Y-2*X=1 ] ).
 
