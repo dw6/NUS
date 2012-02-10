5b27bc54c3898917da2c7073907b98a6cf5a1da0
Benjamin Tan <ben@witsvale.com>
2012-02-11 01:51:14 +0800

    some nice progress, but still super buggy

--- CS4216/PS1/ex2.pl
+++ CS4216/PS1/ex2.pl
@@ -1,24 +1,57 @@
-is_term(X * Y) :- number(X), var(Y).
-n_term(X * Y, X * Y) :- number(X), !.
-n_term(X * Y, Y * X) :- number(Y), !.
-
-n_term(InX+InY, OutX+OutY) :- !,
-	n_term(InX, AuxX), n_term(InY, AuxY), n_term(AuxX,OutX), n_term(AuxY,OutY).
-n_term(InX-InY, OutX-OutY) :- !,
-	n_term(InX, AuxX), n_term(InY, AuxY), n_term(AuxX,OutX), n_term(AuxY,OutY).
-
-eqn(InX+InY=Z, OutX+OutY=Z) :- !, n_term(InX,OutX), n_term(InY,OutY).
-eqn(InX-InY=Z, OutX-OutY=Z) :- !, n_term(InX,OutX), n_term(InY,OutY).
 
 
+gauss([],[]) :- !.
+
+gauss(AugMx,AugMx) :- length(AugMx, 1), !.
+
+gauss(MatrixIn,[Pivot|MatrixAuxOut]) :- !,
+	get_pivot(MatrixIn,Pivot,MatrixRest), 
+	apply_pivot_matrix(Pivot,MatrixRest,MatrixAux), writeln(MatrixAux),
+	gauss(MatrixAux,MatrixAuxOut).
 
 
+% Apply pivot to entire matrix.
+apply_pivot_matrix(P,[M|MT],[R|RT]) :- !,
+	apply_pivot_row(P,M,R),
+	apply_pivot_matrix(P,MT,RT).
+apply_pivot_matrix(_,[],[]) :- !.
 
-%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+% Apply Pivot row P onto R, giving RO. 
+% Assume that P and R have been normalized.
+apply_pivot_row(P,R,RO) :- 
+	norm_row(P,PP), norm_row(R,RR), 
+	apply_pivot_h(PP,RR,[_|RO]).
+
+% apply_pivot_h([1,2,3], [1,-2,-3], X).
+% X = [0, 4, 6].
+apply_pivot_h(_,RIn,ROut) :- RIn = [Z|_], Z =:= 0, ROut = RIn.
+apply_pivot_h([PH|PT],[H|T],[RH|RT]) :- RH is PH-H, apply_pivot_h(PT,T,RT), !.
+apply_pivot_h(_,[],[]) :- !.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+% Returns the first leading non zero in the matrix.
+% get_pivot([[0,0,0,1], [4,0,0,3], [2,3,4,5]], X).
+% X = [4, 0, 0, 3].
+get_pivot(M,P,Rest) :- get_pivot_h(M,P), subtract(M, [P], Rest).
+get_pivot_h([R|RT],Pivot) :- R = [Z|_], Z =:= 0, get_pivot_h(RT, Pivot), !.
+get_pivot_h([R|_],R):- !.
+get_pivot_h([],_):- !.	
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+% Normalize row.
+% norm_row([-2, 3, -4], X) => X = [1.0, -1.5, 2.0].
+norm_row(R,PR) :- R = [Z|T], Z =:= 0, norm_row(T,PR), !.
+norm_row(R,PR) :- R = [H|_], H2 is 1/H, norm_row(H2,R,PR), !.
+norm_row(F,[H|T],[P|PT]) :- !, P is H*F, norm_row(F,T,PT).
+norm_row(_,[],[]) :- !. 
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 % Creates an augmented matrix based on the equations.
-%
-% ?- aug_rows([-1*X-2*B+3*C-4*D-5*R, -4*A-6*R-6*T+5*B], Out).
-%    Out = [[-1, -2, 3, -4, -5, 0, 0], [0, 5, 0, 0, -6, -4, -6]].
+% aug_rows([-1*X-2*B+3*C-4*D-5*R = 10, -4*A-6*R-6*T+5*B = 20], Out).
+% Out = [[-1, -2, 3, -4, -5, 0, 0, 10], [0, 5, 0, 0, -6, -4, -6, 20]].
 aug_rows(Eqn,AugMatrix) :- !, get_vars(Eqn, V), aug_rows(Eqn,V,AugMatrix).
 
 % Appends the = X part to result coefficient list.
@@ -38,11 +71,9 @@ get_vars(Terms=_, InL, OutL) :- !, get_vars(Terms,InL,OutL).
 get_vars([H|T],InL,OutL) :- !, get_vars(H,InL,AuxL), get_vars(T,AuxL, OutL).
 get_vars([],L,L) :- !.
 
-%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 % Retrieve coefficients of the equation given the (sorted) variables list.
-% 
-% ?- get_coeff(B, -1*X-2*B+3*C, Out).
-% Out = -2.
+% ?- get_coeff(B, -1*X-2*B+3*C, Out) => Out = -2.
 get_coeff(V1,N*V2,N) :- V1 == V2, !.
 get_coeff(V1,_A+N*V2,N) :- V1 == V2, !.
 get_coeff(V1,_A-N*V2,N1) :- V1 == V2, N1 is N * -1, !.
@@ -50,15 +81,12 @@ get_coeff(Var,A+_B,N) :- get_coeff(Var,A,N),!.
 get_coeff(Var,A-_B,N) :- get_coeff(Var,A,N),!.
 get_coeff(_,_,0).
 
-
-	
-
 % solve_lin( [ X*2+Y=3, 3*Y-2*X=1 ] ).
 
 
 
 
-
+:- gauss([[2,1,3],[-2,3,1]], X), writeln(X).
 
 
 
