186e43854417dfd1ae402896db729c483f26e976
Benjamin Tan <ben@witsvale.com>
2012-02-11 15:11:20 +0800

    completed ps2, with known bugs

--- CS4216/PS1/ex2.pl
+++ CS4216/PS1/ex2.pl
@@ -1,28 +1,26 @@
+% Benjamin Tan Wei Hao
+% U077129N
+% PS2 - Exercise 2
+
+% Known bugs: Doesn't handle missing variables properly.
+
 solve_lin(Eqns) :-
 	get_vars(Eqns,OutV), 
 	aug_rows(Eqns,AugMx),
 	gauss(AugMx,Mx),
-	solver(Mx,OutV).
-
+	solver(Mx,OutV), !.
 
 rhs_sum(A,B, X) :- !, zip(A,B,C), sum(C,X).	
 zip([A|AT],[B|BT],[C|CT]) :- !, C is A * B, zip(AT,BT,CT).
 zip([],[],[]) :- !.
 
 sum([], 0).
-sum([H|T], S) :- sum(T, S1), S is H + S1.
-
+sum([H|T],S) :- sum(T,S1), S is H + S1.
 
 % Given an augmented matrix, and a list of variables, solve the system of eqns.
 solver(AugM,Vars) :- reverse(AugM,AR), reverse(Vars,VR), solver(AR,VR,[]).
-
-% PH|PT -> Previous Answer
 solver([H|T],[VH|VT],PIn) :- solve(H,VH,PIn,POut), solver(T,VT,POut).
-
 solver([],[],_) :- !.
-
-
-
 
 solve(E,V,VIn,VOut) :-
 	length(E,2) -> 
@@ -36,14 +34,7 @@ solve(E,V,VIn,VOut) :-
 			V is (Z-RHS)/EH,
 			append(VIn,[V],VOut)
 		).
-
 solve([],_,_,_) :- !.
-
-% solve(E,In,X) :- 
-% 	writeln(E), length(E,3) -> E = [A,B,C], X is (C-In*B)/A ; E = [A,B], X is B/A.
-
-
-
 
 gauss([],[]) :- !.
 gauss(MatrixIn,[Pivot|MatrixAuxOut]) :- 
@@ -107,7 +98,9 @@ aug_row(_,[],[]) :- !.
 
 % Collect unique variables from each of the equations.
 get_vars(InL,OutL) :- !, get_vars(InL, [], AuxL), sort(AuxL,OutL). 
+get_vars(C,InL,OutL) :- var(C), append([C],InL, OutL).
 get_vars(N*C,InL,OutL) :- number(N), var(C), append([C],InL, OutL).
+get_vars(C*N,InL,OutL) :- number(N), var(C), append([C],InL, OutL).
 get_vars(X+Y,InL,OutL) :- !, get_vars(X,InL,AuxL), get_vars(Y,AuxL,OutL).
 get_vars(X-Y,InL,OutL) :- !, get_vars(X,InL,AuxL), get_vars(Y,AuxL,OutL).
 get_vars(Terms=_, InL, OutL) :- !, get_vars(Terms,InL,OutL).
@@ -117,9 +110,15 @@ get_vars([],L,L) :- !.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 % Retrieve coefficients of the equation given the (sorted) variables list.
 % ?- get_coeff(B, -1*X-2*B+3*C, Out) => Out = -2.
+get_coeff(V1,V2,1) :- V1 == V2, !.
 get_coeff(V1,N*V2,N) :- V1 == V2, !.
+get_coeff(V1,V2*N,N) :- V1 == V2, !.
 get_coeff(V1,_A+N*V2,N) :- V1 == V2, !.
 get_coeff(V1,_A-N*V2,N1) :- V1 == V2, N1 is N * -1, !.
+get_coeff(V1,_A+V2,1) :- V1 == V2, !.
+get_coeff(V1,_A-V2,-1) :- V1 == V2, !.
+get_coeff(V1,_A+V2*N,N) :- V1 == V2, !.
+get_coeff(V1,_A-V2*N,N1) :- V1 == V2, N1 is N * -1, !.
 get_coeff(Var,A+_B,N) :- get_coeff(Var,A,N),!.
 get_coeff(Var,A-_B,N) :- get_coeff(Var,A,N),!.
 get_coeff(_,_,0).
@@ -135,9 +134,13 @@ get_coeff(_,_,0).
 
 
 % Test Cases 
-% solve_lin( [ 2*X+1*Y=3, 3*Y-2*X=1 ] ).
-% solve_lin([3*X+4*Y+1*Z=6, 2*X-1*Y+2*Z = -5, 1*X+3*Y-1*Z=9]).   % (X,Y,Z)=(2,1,-4)
-% solve_lin([2*X-3*Y+1*Z = -5, 3*X+2*Y-1*Z = 7,1*X+4*Y-5*Z = 3]). % (X,Y,Z)=(1,3,2)
-% solve_lin([1*X-1*Y+0*Z = 2, 2*X-1*Y-1*Z = 3, 1*X+1*Y+1*Z = 6]). % (X,Y,Z)=(3,1,2)
-% solve_lin([1*X-3*Y+1*Z = 4,2*X-8*Y+8*Z = -2,-6*X+3*Y-15*Z = 9]). % (X,Y,Z)=(3,-1,-2)
-
+:- solve_lin([2*X+1*Y=3, 3*Y-2*X=1]), 
+   write('X='),writeln(X),write('Y='),writeln(Y),nl.
+:- solve_lin([3*X+4*Y+1*Z=6, 2*X-1*Y+2*Z = -5, 1*X+3*Y-1*Z=9]),
+   write('X='),writeln(X),write('Y='),writeln(Y),write('Z='),writeln(Z),nl.   % (X,Y,Z)=(2,1,-4)
+:- solve_lin([2*X-3*Y+1*Z = -5, 3*X+2*Y-1*Z = 7,1*X+4*Y-5*Z = 3]),
+   write('X='),writeln(X),write('Y='),writeln(Y),write('Z='),writeln(Z),nl. % (X,Y,Z)=(1,3,2)
+:- solve_lin([1*X-1*Y+0*Z = 2, 2*X-1*Y-1*Z = 3, 1*X+1*Y+1*Z = 6]),
+   write('X='),writeln(X),write('Y='),writeln(Y),write('Z='),writeln(Z),nl. % (X,Y,Z)=(3,1,2)
+:- solve_lin([1*X-3*Y+1*Z = 4,2*X-8*Y+8*Z = -2,-6*X+3*Y-15*Z = 9]),
+   write('X='),writeln(X),write('Y='),writeln(Y),write('Z='),writeln(Z),nl. % (X,Y,Z)=(3,-1,-2)
