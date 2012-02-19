4a363d6d94ec34d03ba3db328484b71c7c72b26b
Benjamin Tan <ben@witsvale.com>
2012-02-19 23:23:53 +0800

    tiny changes

--- CS4216/PS2/ex3.pl
+++ CS4216/PS2/ex3.pl
@@ -1,3 +1,4 @@
+% Benjamin Tan Wei Hao U077129N
 :- lib(matrix_util).
 
 % Formats input arguments into a list.
@@ -6,22 +7,17 @@ to_list((X,Y),[X|T]) :- !, to_list(Y,T).
 to_list(X,[X]) :- !.
 
 % Split domains into its Variables, and its allowable Values
-% dom_ran([X:3..10,Y:4..10], V, L).
-% V = [X, Y], L = [[3, 4, 5, 6, 7, 8, 9, 10], [4, 5, 6, 7, 8, 9, 10]]
 dom_ran([V:Low..High|Rest], [V|VR], [L|LR]) :- 
 	(foreach(E,L), count(I,Low,High) do E = I),
 	dom_ran(Rest,VR,LR).
 dom_ran([],[],[]) :- !.
 
-
 solve(Vars,Doms,Cons,Solns) :-
 	length(Vars,Len),length(Solns,Len),
-	(fromto((Vars,Doms,Solns),([V|VR],[D|DR],[S|SR]),(VR,DR,SR),([],[],[])) do ( member(S,D), V is S)),
+	(fromto((Vars,Doms,Solns),([V|VR],[D|DR],[S|SR]),(VR,DR,SR),([],[],[])) do ( member(S,D), V is S )),
 	(foreach(C,Cons) do C).
 
-% solve_all([X,Y],[[1,2,3,4],[1,2,3,4]],[1*X + 2*Y =:= 6],S).
 solve_all(Vars,Doms,Cons,Solns) :- findall(Soln, solve(Vars,Doms,Cons,Soln),Solns).
-
 
 % propagate( { 1*X+1*Y=:=4*Z, 1*Y > 1*X }, {X:1..4, Y:2..5, Z:1..1}, NewDomains).
 propagate(LC,DC,NDoms) :-
@@ -30,29 +26,3 @@ propagate(LC,DC,NDoms) :-
 	transpose(Solns,TSolns),
 	(fromto((Vars,TSolns,NDoms),([V|VT],[D|DT],[{V::Low..High}|NDT]),(VT,DT,NDT),([],[],[]))
 	do ( min(D,Low), max(D,High) )).	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
