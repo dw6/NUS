0d50786474b7e2c87b79ac93afea17b870617ffc
Benjamin Tan Wei Hao <ben@witsvale.com>
2012-03-04 22:41:24 +0800

    still chugging along

--- CS4216/PS3/ex2.pl
+++ CS4216/PS3/ex2.pl
@@ -23,30 +23,37 @@ convert(Board,BoardList) :-
 search(Board,CarvedPos) :-
   dim(Board,[N,N]),
   no_of_tiles(N,CarvedPos,T),
-  flatten_array(Board,FlatBoard),
-  (foreacharg(Arg,FlatBoard), param(FlatBoard,T)
+  (foreacharg(Arg,Board), param(T)
   do
-    select_val(1,T,Arg)
-  ),
-  writeln(Board).
+  (
+    (foreacharg(A,Arg),param(T)
+    do
+    (
+      atom(A) -> true; select_val(1,T,A)
+    )) 
+  )).
+  
+  
 
 constraints(Board,CarvedPos) :-
   dim(Board,[N,N]),
   no_of_tiles(N,CarvedPos,T), % Find num of tiles. 1..T is therefore the domain.
-  ( multifor([X,Y],1,N), param(Board,N,T)
+  ( multifor([X,Y],1,N), param(Board,N,CarvedPos,T)
   do
-    flatten_array(Board,FlatBoard), FlatBoard::1..T,
     (
-     ((X1 $= X+1, Y1 $= Y);
-      (X1 $= X-1, Y1 $= Y);
-      (X1 $= X, Y1 $= Y+1);
-      (X1 $= X, Y1 $= Y-1))
-     ,[X1,Y1]::1..N,
-      not member(X-Y,CarvedPos), 
-      not member(X1-Y1,CarvedPos)) ->   % None of the positions are on an 'x'
-      Board[X,Y] $= Board[X1,Y1]
-    ; true
-  ).
+       
+      (not member(X-Y,CarvedPos)) ->   
+       (
+          V::1..T,subscript(Board,[X,Y],V),[X1,Y1]::1..N,
+          (X1 $= X+1, Y1 $= Y, X1 $\= X,   X1 $\= X-1, Y1 $\= Y-1,Y1 $\= Y+1) or
+          (X1 $= X-1, Y1 $= Y, X1 $\= X,   X1 $\= X+1, Y1 $\= Y-1,Y1 $\= Y+1) or
+          (X1 $= X, Y1 $= Y+1, X1 $\= X-1, X1 $\= X+1, Y1 $\= Y-1,Y1 $\= Y  ) or
+          (X1 $= X, Y1 $= Y-1, X1 $\= X-1, X1 $\= X+1, Y1 $\= Y,  Y1 $\= Y+1)
+        )
+      ; true
+    )
+  ). 
+  
 
 
 no_of_tiles(N,CarvedPos,Count) :-
@@ -59,10 +66,22 @@ select_val(K,Number,Selection) :-
   K = Number, K1 is K+1,
   select_val(K1,Number,Selection).
 
+delete_all([],_,[]).
+delete_all([H|T],A,Result) :- H=A, delete_all(T,A,Result).
+delete_all([H|T],A,[H|Result]) :- delete_all(T,A,Result).
 
 % Fill Board with 'x' at Positions
 carve_squares(CarvedPos,Board) :-
   foreach(X-Y,CarvedPos),param(Board)
   do ( subscript(Board,[X,Y],x) ).
+
+memberlist([],_).
+memberlist([H|T],L) :- member(H,L), memberlist(T,L).
+
+sorted([]).
+sorted([_]).
+sorted([H1,H2|T]) :- H1 $< H2, sorted([H2|T]).
+
+distinct(K,L) :- length(M,K), sorted(M),memberlist(M,L), memberlist(L,M).
 
 % solve(5,[1-5,4-5,5-1],Result).
