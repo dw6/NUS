573587276fd27107d9014aafa9ae0b40aa27dd6b
Benjamin Tan Wei Hao <ben@witsvale.com>
2012-03-27 02:23:51 +0800

    initial version. optimized from 120 seconds to 7 seconds

---
+++ CS4216/PS5/ex2.pl
@@ -0,0 +1,22 @@
+%%%%%%%%%%%%%%%%%%%%%%%
+% Benjamin Tan Wei Hao
+% PS 5 
+%%%%%%%%%%%%%%%%%%%%%%%
+:- lib(ic).
+
+solve(L,B) :-
+	B1 is B-1,
+	length(LR,B),
+	LR::0..B1,
+	reverse(L,LR),
+	% Speeds things up significantly
+	sum(L) #= B,	
+	(foreach(E,L), count(I,0,B1), param(L) do writeln(L), occ(L,I,E)).
+
+% Constraint version of occurs.
+occ([],_,N) :- N is 0.
+occ([H|T],X,N) :- occ(T,X,N1), H #= X,  N #= N1+1.
+occ([H|T],X,N) :- occ(T,X,N1), H #\= X, N #= N1.
+
+:- solve(L,10).
+halt.
