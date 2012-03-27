0d9af45a8d29d7c9c8116667ed57ce7b6670095c
Benjamin Tan Wei Hao <ben@witsvale.com>
2012-03-28 01:11:31 +0800

    added lookupInClass in Wrapper's prologue

--- CS4215/cs4215_week_10/oPL/Wrapper.java
+++ CS4215/cs4215_week_10/oPL/Wrapper.java
@@ -3,13 +3,16 @@ package oPL;
 public class Wrapper
 {
 	public static String prologue = 
-		"let new = fun theClass -> [Class: theClass] end " +
-		"	 lookup = fun object methodName -> object.Class.methodname end" +		
-		" in ";
-
-
-
-					
+		"let lookupInClass = recfun lookupInClass theClass methodname ->" +
+		"						if theClass hasproperty methodname" +
+		"						then theClass.methodname" +
+		"						else (lookupInClass theClass.Parent methodname)" +
+		"						end" +
+		"					  end" +
+		" in " +
+		"	let new = fun theClass -> [Class: theClass] end " +
+		"		lookup = fun object methodName -> object.Class.methodname end" +		
+		" 	in ";
 	
-	public static String epilogue = " end";
+	public static String epilogue = " end end";
 }
