83c939cd16e4de3f911a6ed34585b37f95a956db
Benjamin Tan Wei Hao <ben@witsvale.com>
2012-03-30 23:49:28 +0800

    initial commit of week 11's lab

--- CS4215/cs4215_week_10/oPL/LookupApplication.java
+++ CS4215/cs4215_week_10/oPL/LookupApplication.java
@@ -3,108 +3,44 @@ package oPL;
 import java.util.*;
 
 public class LookupApplication extends Application
-{
-	class ClassAndMethodValue
-	{
-		String className  = "";
-		String methodName = "";
-		ClassAndMethodValue(String className, String methodName)
-		{
-			this.className  = className;
-			this.methodName = methodName;
-		}
-		
-		@Override
-		public int hashCode()
-		{
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((className == null) ? 0 : className.hashCode());
-			result = prime * result + ((methodName == null) ? 0 : methodName.hashCode());
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj)
-		{
-			ClassAndMethodValue other = (ClassAndMethodValue) obj;
-			if(this.className.equals(other.className) && this.methodName.equals(other.methodName))
-			{
-				return true;
-			}
-			return false;
-		}
-
-
-		private LookupApplication getOuterType()
-		{
-			return LookupApplication.this;
-		}
-	}
-	
-	public String operator;
-	public Vector<Expression> operands;
-	public static Hashtable<ClassAndMethodValue,FunValue> cache = new Hashtable<ClassAndMethodValue,FunValue>();
-
-	// 1. Find the class of the object.
-	
-	
-	
-	// 2. Find the method name from operands
-	
-	
+{	
+	RecordValue theClass;
+	FunValue theMethod;
 	
 	public LookupApplication(Expression rator, Vector<Expression> rands)
 	{
 		// The operator is ALWAYS lookup
 		super(rator, rands); // Seems like no effect.
-		operator = rator.toString();
-		operands = rands;
+		theClass  = null;
+		theMethod = null;
 	}
 	
 	public Value eval(Environment e)
 	{
-		
 		// The first operand is always "this"
 		// Locate the address of the object.
-		String thisObj = operands.get(0).toString();
+		String thisObj = super.operands.get(0).toString();
 		int objAddr = e.access(thisObj);
 				
 		// Get the objClass from the store.
 		RecordValue objRecord = (RecordValue)Store.theStore.get(objAddr);		
-		RecordValue theClass = (RecordValue)Store.theStore.get(objRecord.get("Class"));
-						
-		FunValue lookupResult = 
-				cache.get(new ClassAndMethodValue(theClass.toString(), operands.get(1).toString()));
-				
-		FunValue theMethod = null;
-				
-		if(lookupResult == null)
+		RecordValue objClass = (RecordValue)Store.theStore.get(objRecord.get("Class"));
+		
+		
+		// When do we need to cache?
+		if (theClass == null || theClass != objClass)
 		{
-			// here we perform the actual lookup.
-			// 1. First, we have to locate the function called 'lookup'
-			FunValue lookupFun = (FunValue)Store.theStore.get(e.access(operator));
-			
-			// 2. Second, we run the actual lookup.
-			
-			// For this (the obj)
-			int newLoc = Store.theStore.newLocation();
-			Store.theStore.extend(newLoc, objRecord);
-			lookupFun.environment = lookupFun.environment.extend(lookupFun.formals.firstElement(), newLoc);
-			
-			// For methodname
-			newLoc = Store.theStore.newLocation();			
-			Store.theStore.extend(newLoc, operands.get(1).eval(e));	
-			lookupFun.environment = lookupFun.environment.extend(lookupFun.formals.lastElement(), newLoc);
-					
-			theMethod = (FunValue)lookupFun.body.eval(lookupFun.environment);
-			cache.put(new ClassAndMethodValue(theClass.toString(), operands.get(1).toString()), theMethod);	
+			// Cache miss
+			// Perform lookup and update the cache
+			theMethod = (FunValue) super.eval(e);
+			theClass = objClass;
+			return theMethod;
 		}
 		else
 		{
-			theMethod = lookupResult;
-		}		
-		return theMethod;
+			// Cache hit
+			return theMethod;
+		}
 	}
 
 	
